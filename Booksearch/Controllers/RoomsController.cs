2bac8f41b8cc22ccd690f37807da674b7acb22c6
Roro7 <roselle.ali@student.hv.se>
2026-03-03 19:46:37 +0100

    Added RoomsController and BokaGrupprum page with custom CSS

---
+++ Booksearch/Controllers/RoomsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Booksearch.Controllers;
+
+public class RoomsController : Controller
+{
+    // GET
+    public IActionResult BokaGrupprum()
+    {
+        return View();
+    }
+}
