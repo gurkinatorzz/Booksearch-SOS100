6a9cf0a72277279f2dc88f04e4a01f3ee4ec5580
Williamberge123 <wibe0005@student.hv.se>
2026-03-16 14:21:10 +0100

    Lagt till Databas

---
+++ BookLoanService/Models/BookLoan.cs
@@ -0,0 +1,20 @@
+﻿namespace BookLoanService.Models;
+
+public class BookLoan
+{
+    public int Id { get; set; }
+
+    public int BookId { get; set; }
+
+    public string BookTitle { get; set; } = string.Empty;
+
+    public string BorrowerName { get; set; } = string.Empty;
+
+    public DateTime LoanDate { get; set; }
+
+    public DateTime DueDate { get; set; }
+
+    public DateTime? ReturnedDate { get; set; }
+    
+    public bool IsComplete  { get; set; }
+}
