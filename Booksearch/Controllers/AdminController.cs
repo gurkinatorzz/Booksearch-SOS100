b8717386427432b3ed8208a32750208cf0011fbf
Axel <absalon1066@protonmail.com>
2026-03-16 22:21:20 +0100

    etc

---
+++ Booksearch/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+namespace Admin.Controllers;
+ 
+[Authorize]
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
